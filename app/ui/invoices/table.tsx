b03df589a7d5025329100d56be8da4cafa823aa6
Phi Dinh <phidv9855@gmail.com>
2025-02-25 00:28:24 +0700

    ref: chapter-13 add error handling

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from "next/image";
+import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
 export default async function InvoicesTable({
   query,
   currentPage,
 }: {
-  query: string;
-  currentPage: number;
+  readonly query: string;
+  readonly currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
